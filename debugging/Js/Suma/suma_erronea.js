ef38aa4da25276c55f6b3c21c1368f769d3e08ab
Your Name <you@example.com>
2024-12-12 19:42:36 +0100

    arreglado

--- debugging/Js/Suma/suma_erronea.js
+++ debugging/Js/Suma/suma_erronea.js
@@ -1,6 +1,6 @@
 function calcularSuma() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
+    const num1 = parseFloat(document.getElementById('num1').value);  // Convertimos a número
+    const num2 = parseFloat(document.getElementById('num2').value);  // Convertimos a número
     const suma = num1 + num2; 
     document.getElementById('resultado').innerText = `La suma es: ${suma}`;
 }
